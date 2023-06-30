70446c593a00776fa42eb778aab032fe4ba0de6e
Ahmet Tunahan Bekdaş <97003033+ahmettunahanbekdas@users.noreply.github.com>
2023-06-30 21:46:12 +0300

    Initial Commit

---
+++ Story/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Story
+//
+//  Created by Ahmet Tunahan Bekdaş on 30.06.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
